774e17ac4ac6036eb79f528fd92f60bd92642835
Anastasiia <malinina.a@gmail.com>
2023-02-15 19:31:21 +0200

    Спринт 3: Добавлен Лэйбл со счетчиком и кнопка для его увеличения.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -2,18 +2,26 @@
 //  ViewController.swift
 //  Counter
 //
-//  Created by Админ on 13.02.2023.
+//  Created by Azonaz on 13.02.2023.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var viewLabel: UILabel!
+    @IBOutlet weak var tapButton: UIButton!
+    var count = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func tapButton(_ sender: Any) {
+        count += 1
+        viewLabel.numberOfLines = 3
+        viewLabel.lineBreakMode = .byWordWrapping
+        viewLabel.text = "Значение счетчика: \n \n \(count)"
+    }
+   
 }
 
